e14405148b34d1bb0a82abbadf261ccdbc9ff906
Sang Khánh Vinh <khanhvinhit@gmail.com>
2020-05-12 20:47:10 +0700

    v

--- DGDRL/DGDRL.APP/GUI/usDanhSachDanhGiaDiemRenLuyen.cs
+++ DGDRL/DGDRL.APP/GUI/usDanhSachDanhGiaDiemRenLuyen.cs
@@ -15,6 +15,33 @@ namespace DGDRL.APP.GUI
         public usDanhSachDanhGiaDiemRenLuyen()
         {
             InitializeComponent();
+            LoadHocKy();
+            LoadNamHoc();
+        }
+        public void LoadHocKy()
+        {
+
+            var lst = new List<KeyValuePair<int, string>>() {
+                new KeyValuePair<int, string>(1,"Học Kỳ 1"),
+                new KeyValuePair<int, string>(2,"Học Kỳ 2")
+            };
+            cbbHocKy.Properties.DataSource = lst;
+            cbbHocKy.Properties.ValueMember = "Key";
+            cbbHocKy.Properties.DisplayMember = "Value";
+            cbbHocKy.EditValue = "1";
+        }
+        public void LoadNamHoc()
+        {
+            var lst = new List<KeyValuePair<string, string>>();
+            for (int i = DateTime.Now.Year; i >= DateTime.Now.AddYears(-10).Year; i--)
+            {
+                var item = i + "-" + (i + 1);
+                lst.Add(new KeyValuePair<string, string>(item, item));
+            }
+            cbbNamHoc.Properties.DataSource = lst;
+            cbbNamHoc.Properties.ValueMember = "Key";
+            cbbNamHoc.Properties.DisplayMember = "Value";
+            cbbNamHoc.EditValue = (DateTime.Now.Year + "-"+ DateTime.Now.AddYears(1).Year);
         }
     }
 }
