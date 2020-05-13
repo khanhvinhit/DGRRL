7ec321c373544ddd862db0da590cf50a87d26acb
Sang Khánh Vinh <khanhvinhit@gmail.com>
2020-05-13 20:16:08 +0700

    Update usDanhSachDanhGiaDiemRenLuyen.Designer.cs

--- DGDRL/DGDRL.APP/GUI/usDanhSachDanhGiaDiemRenLuyen.Designer.cs
+++ DGDRL/DGDRL.APP/GUI/usDanhSachDanhGiaDiemRenLuyen.Designer.cs
@@ -28,10 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.labelControl6 = new DevExpress.XtraEditors.LabelControl();
+            this.cbbNamHoc = new DevExpress.XtraEditors.LookUpEdit();
+            this.cbbHocKy = new DevExpress.XtraEditors.LookUpEdit();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.cbbNamHoc.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cbbHocKy.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.labelControl1);
+            this.panelControl1.Controls.Add(this.cbbHocKy);
+            this.panelControl1.Controls.Add(this.labelControl6);
+            this.panelControl1.Controls.Add(this.cbbNamHoc);
+            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelControl1.Location = new System.Drawing.Point(0, 0);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(952, 485);
+            this.panelControl1.TabIndex = 0;
+            // 
+            // labelControl6
+            // 
+            this.labelControl6.Location = new System.Drawing.Point(5, 5);
+            this.labelControl6.Name = "labelControl6";
+            this.labelControl6.Size = new System.Drawing.Size(42, 13);
+            this.labelControl6.TabIndex = 12;
+            this.labelControl6.Text = "Năm Học";
+            // 
+            // cbbNamHoc
+            // 
+            this.cbbNamHoc.Location = new System.Drawing.Point(5, 24);
+            this.cbbNamHoc.Name = "cbbNamHoc";
+            this.cbbNamHoc.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.cbbNamHoc.Properties.NullText = "";
+            this.cbbNamHoc.Properties.PopupSizeable = false;
+            this.cbbNamHoc.Size = new System.Drawing.Size(304, 20);
+            this.cbbNamHoc.TabIndex = 13;
+            // 
+            // cbbHocKy
+            // 
+            this.cbbHocKy.Location = new System.Drawing.Point(315, 24);
+            this.cbbHocKy.Name = "cbbHocKy";
+            this.cbbHocKy.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.cbbHocKy.Properties.NullText = "";
+            this.cbbHocKy.Properties.PopupSizeable = false;
+            this.cbbHocKy.Size = new System.Drawing.Size(304, 20);
+            this.cbbHocKy.TabIndex = 13;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(315, 5);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(33, 13);
+            this.labelControl1.TabIndex = 12;
+            this.labelControl1.Text = "Học Kỳ";
+            // 
+            // usDanhSachDanhGiaDiemRenLuyen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panelControl1);
+            this.Name = "usDanhSachDanhGiaDiemRenLuyen";
+            this.Size = new System.Drawing.Size(952, 485);
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            this.panelControl1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.cbbNamHoc.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cbbHocKy.Properties)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LookUpEdit cbbHocKy;
+        private DevExpress.XtraEditors.LabelControl labelControl6;
+        private DevExpress.XtraEditors.LookUpEdit cbbNamHoc;
     }
 }
